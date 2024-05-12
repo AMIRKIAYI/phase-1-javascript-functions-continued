0b628175f833a3463fa8feb4c57f10adbe9f1c3a
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-12 12:30:10 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+saturdayFun()
+let mondayWork = function (activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+
+}
+mondayWork()
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
+// Test the function
+const highlightAdjective = wrapAdjective('*');
+console.log(highlightAdjective('a hard worker')); 
+// function wrapAdjective(me ="You are"){
+//     return function(message, symbol){
+//         return`${me} ${symbol}${message}${symbol}!`
+
+//     }
+// }
+// wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+    
